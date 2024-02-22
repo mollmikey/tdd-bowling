25bd82cdcd7ed6a605dc86ebc14e73110122b396
Michael <104609759+mollmikey@users.noreply.github.com>
2024-02-22 15:05:56 +1300

    gutterball test completed, red light

---
+++ score.test.js
@@ -0,0 +1,14 @@
+import { test, expect } from 'vitest'
+import * as score from './score' // this is the line to add
+
+test.skip('test setup working', () => {
+  expect(true).toBeTruthy()
+})
+
+test('score a gutterball frame', () => {
+  const frame = [0, 0]
+  const expected = 0
+  const actual = score.scoreFrame(frame)
+
+  expect(actual).toBe(expected)
+})
--- tests/score.test.js
+++
@@ -1,5 +0,0 @@
-import { test, expect } from 'vitest'
-
-test('test setup working', () => {
-  expect(true).toBeTruthy()
-})
