4a0353c1a590cf4d6316d9aa6c292bace06964a0
Kate <63078091+Katie-Davies@users.noreply.github.com>
2024-02-22 15:40:22 +1300

    refactored spareFrame

--- score.js
+++ score.js
@@ -1,7 +1,11 @@
 export { scoreFrame }
 
 function scoreFrame(frame) {
-  return 11
+  let total = 0
+  for (let i = 0; i < frame.length; i++) {
+    total += frame[i][0] + frame[i][1] + frame[i + 1][0]
+    return total
+  }
 }
 /*gutterBall*/
 //frame[0] + frame[1]
